45fea33cebdeac4474a90c7bffdd5933811ed305
Adrian-ortiz0 <dxniel7328@gmail.com>
2024-09-21 09:10:23 -0500

    feat: :sparkles: I've added the functionality to filter gossips by category

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 let chismes = [];
 
+//---------------------------------------------------------
+
+// Cargar y guardar
+
 function guardarChismes() {
   localStorage.setItem("chismes", JSON.stringify(chismes));
 }
@@ -13,6 +17,10 @@ function cargarChismes() {
   }
 }
 
+//---------------------------------------------------------
+
+// LLamadas de eventos
+
 const chismeForm = document.getElementById("chismeForm");
 chismeForm.addEventListener("click", function (e) {
   e.preventDefault();
@@ -22,6 +30,15 @@ const btnGuardarChisme = document.getElementById("btnGuardarChisme");
 btnGuardarChisme.addEventListener("click", function () {
   guardandoChisme();
 });
+
+const filtroCategoria = document.getElementById("filtroCategoria");
+filtroCategoria.addEventListener("change", function () {
+  filtrarChismes();
+});
+
+//------------------------------------------------------
+
+// Funciones
 
 function guardandoChisme() {
   cargarChismes();
@@ -56,4 +73,65 @@ function guardandoChisme() {
   };
   chismes.push(newChisme);
   guardarChismes();
+  alert("Chisme registrad con exito!");
+  mostrarChismes();
+}
+
+function mostrarChismes() {
+  cargarChismes();
+  const contenedorDeChismes = document.getElementById("listaDeChismes");
+  contenedorDeChismes.innerHTML = "";
+  const bannerChismes = document.createElement("div");
+  bannerChismes.innerHTML = `<div>ID</div><div>Description</div><div>Category</div><div>Date</div><div>Status</div>`;
+  bannerChismes.classList.add("plantillaChismes");
+  contenedorDeChismes.append(bannerChismes);
+
+  chismes.forEach(function (chisme, id) {
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("plantillaChismes");
+    newDiv.innerHTML = `<div>${id + 1}</div><div>${
+      chisme.descripcion
+    }</div><div>${chisme.categoria}</div><div>${chisme.fecha}</div><div>${
+      chisme.estado
+    }</div>`;
+    contenedorDeChismes.appendChild(newDiv);
+  });
+}
+mostrarChismes();
+
+function actualizarListaChismes(listaFiltrada) {
+  const contenedorDeChismes = document.getElementById("listaDeChismes");
+  contenedorDeChismes.innerHTML = "";
+  const bannerChismes = document.createElement("div");
+  bannerChismes.innerHTML = `<div>ID</div><div>Description</div><div>Category</div><div>Date</div><div>Status</div>`;
+  bannerChismes.classList.add("plantillaChismes");
+  contenedorDeChismes.append(bannerChismes);
+
+  listaFiltrada.forEach(function (chisme, id) {
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("plantillaChismes");
+    newDiv.innerHTML = `<div>${id + 1}</div><div>${
+      chisme.descripcion
+    }</div><div>${chisme.categoria}</div><div>${chisme.fecha}</div><div>${
+      chisme.estado
+    }</div>`;
+    contenedorDeChismes.appendChild(newDiv);
+  });
+}
+
+function filtrarChismes() {
+  cargarChismes();
+  const filtroCategoria = document.getElementById("filtroCategoria").value;
+
+  let chismesEncontrados;
+
+  if (filtroCategoria === "All") {
+    chismesEncontrados = chismes;
+    console.log(chismesEncontrados);
+  } else {
+    chismesEncontrados = chismes.filter(function (chisme) {
+      return chisme.categoria === filtroCategoria;
+    });
+  }
+  actualizarListaChismes(chismesEncontrados);
 }
